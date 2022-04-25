839766a5f85d949d70a31ab0184689af78a61075
Gonzalo Garcia <garciar.gonzalo@gmail.com>
2022-04-25 12:47:10 -0300

    Funciones

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
+let monto = 0
+
+function solicitarMonto () {
+    monto = parseInt(prompt("Ingresá tu monto a invertir"));    
+}
+
+let plazo = 0
+
+function solicitarPlazo () {
+    plazo = parseInt(prompt("Elige tu plazo en meses"));    
+}
 
 let entrada = prompt("Bienvendio a la calculadora de inversiones, ingrese su nombre o escriba ESC para salir.");
-    
+
 while (entrada.toUpperCase() != "ESC") {
     alert ("Hola! " + entrada)
 
-
-var monto = prompt("Ingresá tu monto a invertir");
-var plazo = parseInt(prompt("Elige tu plazo en meses"));
+solicitarMonto ();
+solicitarPlazo ();
 
 if (monto < 1000) {
-    alert ("Mejor gastalo en un vino");
-} else if ((monto =>1000) && (monto <=20000)) {
+    alert ("Mejor gastalo en un vino")
+} else if ((monto >= 1000) && (monto <=20000)) {
     alert ("Hacé un plazo fijo UVA")
-} else if (monto => 20001) {
-    alert ("Comprá dolares");
+} else if (monto >= 20001) {
+    alert ("Comprá dolares")
 }
 
 if (entrada.toUpperCase() == "ESC");
