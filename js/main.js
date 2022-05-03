5520c234d5e7e85692b8b9a3aea59c71a149151b
Gonzalo Garcia <garciar.gonzalo@gmail.com>
2022-05-02 23:45:03 -0300

    Arrays

--- js/main.js
+++ js/main.js
@@ -9,12 +9,31 @@ let plazo = 0
 function solicitarPlazo () {
     plazo = parseInt(prompt("Elige tu plazo en meses"));    
 }
+let inversion = 0
+const inversiones = ["Plazo fijo Uva", "Bonos CER", "Comprar dolares"];
+const tasa = [4,6,8];
+function tuProducto () {
+    inversion = parseInt(prompt("Elegi una opción:" + "1- " + inversiones[0] + " 2- " + inversiones[1] + " 3- " + inversiones[2]));
+    
+    if (inversion == 1) {
+        alert ("Felicitaciones elegiste " + inversiones[0] + " tu tasa de interés es " + tasa[0] + " % mensual.")
+    } else if (inversion == 2) {
+        alert ("Felicitaciones elegiste " + inversiones[1] + " y tu tasa de interés es " + tasa[1] + " % mensual.")
+    } else if (inversion == 3) {
+        alert ("Felicitaciones elegiste " + inversiones[2] + " y tu tasa de interés es " + tasa[2] + " % mensual.")
+    } 
 
-let entrada = prompt("Bienvendio a la calculadora de inversiones, ingrese su nombre o escriba ESC para salir.");
+}
+
+
+
+let entrada = prompt("Bienvenido a la calculadora de inversiones, ingrese su nombre o escriba ESC para salir.");
 
 while (entrada.toUpperCase() != "ESC") {
     alert ("Hola! " + entrada)
 
+
+tuProducto ();
 solicitarMonto ();
 solicitarPlazo ();
 
