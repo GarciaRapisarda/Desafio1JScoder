9bd83e7e77c0546a4219d879e3f4400900747835
Gonzalo Garcia <garciar.gonzalo@gmail.com>
2022-06-02 00:02:56 -0300

    fetch1

--- js/cotizaciones.js
+++ js/cotizaciones.js
@@ -5,8 +5,6 @@ fetch('/data.json')
     .then( (data) => {
  
         data.forEach((valor) => {
-            let div_abuelo = document.createelment("div");
-            div_abuelo.className = "row"
             let div_padre = document.createElement("div");
             div_padre.className = "card row";
         let div_hijo1 = document.createElement("div");
@@ -19,7 +17,6 @@ fetch('/data.json')
         div_hijo2.appendChild(parrafo);
         div_padre.appendChild(div_hijo1);
         div_padre.appendChild(div_hijo2);
-        div_
-        lista.appendChild(div_abuelo);
+        lista.appendChild(div_padre);
     });
     })
