6de61ecda90eba8b414cb52d24efb3f388b50976
Gonzalo Garcia <garciar.gonzalo@gmail.com>
2022-06-08 09:00:27 -0300

    Sección noticias

---
+++ js/noticias.js
@@ -0,0 +1,25 @@
+ 
+fetch('/data.json')
+    .then( (res) => res.json())
+    .then( (respuesta) => {
+        
+let contenido = "";
+
+    for (const data of respuesta) {
+        contenido += `<div class="col-md-4 mt-3">
+        <div class="card">
+        <div class="card-header">
+        ${data.sector}
+        </div>
+        <div class="card-body">
+        <blockquote class="blockquote mb-0">
+        <p>${data.noticia}</p>
+        <footer class="blockquote-footer">Noticia de <cite title="Source Title">${data.fuente}</cite></footer>
+        </blockquote>
+        </div>
+        </div>
+        </div>`;
+    }
+
+    document.getElementById("listado").innerHTML = contenido;
+})
